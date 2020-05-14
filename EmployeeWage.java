da1a5cd256ca32f0d846f26a689da2918f3b4ea2
Ayush <ayushmanojgupta24@gmail.com>
2020-05-14 12:59:24 +0530

    Added functionality to calculate wage of an employee

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -10,7 +10,7 @@ public class EmployeeWage {
 		//Computation
 		double empCheck = Math.floor(Math.random() * 10) % 2;
 		if (empCheck == IS_FULL_TIME)
-			empHrs = 0;
+			empHrs = 8;
 		else
 			empHrs = 0;
 		empWage = empHrs * EMP_RATE_PER_HOUR;
