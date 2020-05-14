18adc3492f62608a2130ea1915600e283bbe8cbc
Ayush <ayushmanojgupta24@gmail.com>
2020-05-14 12:44:29 +0530

    Added while loop to check employee working hours will not exceed to 100 hours or 20 days

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -5,6 +5,7 @@ public class EmployeeWage {
 	public static final int IS_FULL_TIME = 2;
 	public static final int EMP_RATE_PER_HOUR = 20;
 	public static final int NUM_OF_WORKING_DAYS = 20;
+	public static final int MAX_HRS_IN_MONTH = 100;
 
 	public static void main(String[] args) {
 		//Variables
@@ -12,7 +13,7 @@ public class EmployeeWage {
 		//Computation
 		while ( totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS ){
 			totalWorkingDays++;
-			int empHrs = 0, empWage = 0;
+			int empHrs = 0;
 			int empCheck =(int) Math.floor(Math.random() * 10) % 3;
 			switch (empCheck){
 				case IS_FULL_TIME:
@@ -25,7 +26,7 @@ public class EmployeeWage {
 					empHrs = 0;
 			}
 			totalEmpHrs += empHrs;
-      	empWage = empHrs * EMP_RATE_PER_HOUR;
+      	int empWage = empHrs * EMP_RATE_PER_HOUR;
       	totalEmpWage += empWage;
       	System.out.println("Employee Wage : " +empWage);
 		}
