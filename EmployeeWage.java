1fa4c52f308dcb8e9aaef473abb6c0303c271c88
Ayush <ayushmanojgupta24@gmail.com>
2020-05-17 20:35:00 +0530

    manage Employee wage for multiple companies

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,33 +2,18 @@ public class EmployeeWage {
     public static final int IS_FULL_TIME = 1;
     public static final int IS_PART_TIME = 2;
 
-    private int numOfCompanies = 0;
-    private CompanyObj[] companies;
+    public static final int EMP_RATE_PER_HR = 20;
+    public static final int NUM_OF_WORKING_DAYS = 20;
+    public static final int MAX_HRS_IN_MONTH = 100;
 
-    public EmployeeWage(){
-        companies = new CompanyObj[5];
-    }
-
-    void addCompanyEmpWage(String company, int empRatePerHr, int numOfWorkingDays, int maxHoursPerMonth){
-        companies[numOfCompanies] = new CompanyObj(company, empRatePerHr, numOfWorkingDays, maxHoursPerMonth);
-        numOfCompanies++;
-    }
-
-    void totalEmpWage(){
-        for(int i = 0; i<numOfCompanies; i++){
-            companies[i].setTotalEmpWage(this.totalEmpWage(companies[i]));
-            System.out.println(companies[i]);
-        }
-    }
-
-    public int totalEmpWage(CompanyObj companyObj) {
+    static void totalEmpWage(int ratePerHr, int numOfDays, int hrsInMonth) {
         //VARIABLES
         int empHrs = 0;
         int totalEmpHrs = 0;
         int totalWorkingDays = 0;
 
         //COMPUTATION
-        while (totalEmpHrs <= companyObj.maxHoursPerMonth && totalWorkingDays < companyObj.numOfWorkingDays) {
+        while (totalEmpHrs <= hrsInMonth && totalWorkingDays < numOfDays) {
             totalWorkingDays++;
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
             switch (empCheck) {
@@ -45,13 +30,12 @@ public class EmployeeWage {
             totalEmpHrs += empHrs;
             System.out.println("Days: " + totalWorkingDays + " Emp Hours: " + empHrs);
         }
-        return totalEmpHrs * companyObj.empRatePerHr;
+        int totalWage = totalEmpHrs * ratePerHr;
+        System.out.println("Total Emp Wage for company company is " + totalWage);
     }
 
-    public static void main(String[] args){
-        EmployeeWage arrObject = new EmployeeWage();
-        arrObject.addCompanyEmpWage("DMart", 20, 2, 10);
-        arrObject.addCompanyEmpWage("Reliance",50, 5, 15);
-        arrObject.totalEmpWage();
+    public static void main(String args[]) {
+        totalEmpWage(20,20,100);
+        totalEmpWage(15, 25, 120);
     }
 }
